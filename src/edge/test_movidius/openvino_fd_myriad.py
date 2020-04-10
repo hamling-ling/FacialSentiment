7b11eae0c075549d76eb486ba2f369b659a69b3f
hamling-ling <nobubijinesu@gmail.com>
2020-04-10 21:19:24 +0900

    add movidius test code

---
+++ src/edge/test_movidius/openvino_fd_myriad.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+net = cv.dnn_DetectionModel('face-detection-adas-0001.xml',
+                            'face-detection-adas-0001.bin')
+net.setPreferableTarget(cv.dnn.DNN_TARGET_MYRIAD)
+
+frame = cv.imread('sample.bmp')
+if frame is None:
+    raise Exception('Image not found')
+
+_, confidences, boxes = net.detect(frame, confThreshold=0.5)
+
+for confidence, box in zip(list(confidences), boxes):
+    cv.rectangle(frame, box, color=(0, 255, 0))
+    print(confidence, box)
+
+cv.imwrite('out.png', frame)
+
+
