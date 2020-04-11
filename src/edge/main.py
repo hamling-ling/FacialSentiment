0f17a315a7fb1c648420860624a6e7952d3d3b97
hamling-ling <nobubijinesu@gmail.com>
2020-04-11 16:32:49 +0900

    connected detector and classifier

--- src/edge/main.py
+++ src/edge/main.py
@@ -1,6 +1,10 @@
 import shooter as st
 import classifier as cl
 import detector as dt
+import image_utility as util
+import cv2 as cv
+from PIL import Image
+import numpy as np
 
 PATH_DETECTOR_MODEL_XML = 'models/face-detection-adas-0001.xml'
 PATH_DETECTOR_MODEL_BIN = 'models/face-detection-adas-0001.bin'
@@ -17,13 +21,58 @@ def main_loop(detector, classifier):
         break
 
 def process(detector, classifier, stream):
+    boxes = process_face_detection(detector, stream.array)
+
+    sentiments = process_sentiment_analysis(classifier, stream.array, boxes)
+
+    draw_results(stream.array, boxes, sentiments)
+
+def process_face_detection(detector, image):
     # face detection
-    confidences, boxes = detector.detect(stream.array)
-    for confidence, box in zip(list(confidences), boxes):
+    detection_results = detector.detect(image)
+
+    # clipped box list, may contain None
+    clipped_boxes = []
+
+    for res in detection_results:
+        confidence = res[0]
+        box        = res[1]
         print(confidence, box)
 
-    # sengiment analysis
-    # t.b.d
+        clipped = util.clip_box(box, image.shape)
+        if clipped is not None:
+            cv.rectangle(image, clipped, color=(0, 0, 255))
+            clipped_boxes.append(clipped)
+
+    return clipped_boxes
+
+def process_sentiment_analysis(classifier, image, boxes):
+    if(len(boxes) == 0):
+        return None, None
+
+    ret = []
+    for box in boxes:
+        img = util.cropToGray(image, box, 32, classifier.get_input_size())
+
+        # debug
+        #img.save("input_face.png", format='PNG')
+
+        result = classifier.predict(img)
+        ret.append(result)
+    return ret
+
+def draw_results(image, boxes, sentiments):
+    print("draw_result ", len(boxes), len(sentiments))
+    for box, sen in zip(boxes, sentiments):
+        cv.rectangle(image, box, color=(0, 255, 0))
+        if sen is None:
+            continue
+        text = "{} {:.1f}%".format(sen[0], sen[1]*100)
+        print(text)
+        cv.putText(image, text, (box[0], box[1]), cv.FONT_HERSHEY_PLAIN,
+            0.8, (255, 255, 255), 1, cv.LINE_AA)
+
+    cv.imwrite('out.png', image)
 
 def main():
     print('app started')
